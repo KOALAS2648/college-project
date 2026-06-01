8d4bfe4feffe55ad43759ffb9b32ea65ca7f4a06
KOALAS2648 <oliverdevon@icloud.com>
2026-06-01 18:10:46 +0100

    added the player hf

---
+++ hf/P.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <cmath>
+#include <sstream>
+#include <vector>
+#include <SFML/Audio.hpp>
+#include <random>
+
+namespace PlayerCode
+{
+    class Player
+    {
+        public:
+            public:
+            float x;
+            float y;
+            float raduis = 10;
+            sf::Color color = sf::Color::Red;
+			sf::Vector2f position;
+            void draw(sf::RenderWindow* window)
+            {
+                sf::RenderWindow &screen = *window;
+                sf::CircleShape circle1(raduis);
+                circle1.setFillColor(color);
+                circle1.setOrigin(raduis, raduis);
+                circle1.setPosition(x,y);
+                screen.draw(circle1);
+				position = {x, y};
+            }
+    };
+}
